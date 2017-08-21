f75226ebb97aaea8ff751a15ccd4c731fe51ad77
miaoyu <362334256@qq.com>
2017-08-22 00:01:34 +0800

    '提交构建代码'

--- app.js
+++ app.js
@@ -3,23 +3,23 @@
  */
 const koa = require('koa');
 const app = new koa();
-const router = require('koa-router');
-const basicRouter = new router();
+const basicRouter = require('./routers/entry.config.js');
 const { port } = require('./webConfig.json');
 
 
 // 请求前中间件
 // 日志中间件
 app.use(require('./middleware/logger.js')());
-
-
-basicRouter.get('/index', async (ctx) => {
-    ctx.body = '1111'
-})
+// 请求体处理中间件
+app.use(require('koa-bodyparser')({
+    formLimit: '10mb', // 请求表单的最大体积
+    jsonLimit: '10mb', // 请求json的最大体积
+    textLimit: '10mb'  // 请求文本的最大体积
+}));
 
 
 // 装载路由 / 响应前中间件
-app.use(basicRouter.routes(), basicRouter.allowedMethods());
+app.use(basicRouter.routes()).use(basicRouter.allowedMethods());
 
 
 // 启动服务，并输出日期
