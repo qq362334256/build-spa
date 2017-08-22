027bf33de1bc96c21fc5a1802773213398950d4d
miaoyu <362334256@qq.com>
2017-08-23 00:12:33 +0800

    '提交初始构建'

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 /**
  * Created by yanmin.yu on 2017/8/5.
  */
+const path = require('path');
 const koa = require('koa');
 const app = new koa();
-const basicRouter = require('./routers/entry.config.js');
-const { port } = require('./webConfig.json');
+const basicRouter = require('./server/routers/entry.config.js');
+const { host, port, fileCacheTime, queryLimit } = require('./webConfig.json');
 
 
 // 请求前中间件
 // 日志中间件
-app.use(require('./middleware/logger.js')());
+app.use(require('./server/middleware/logger.js')());
+app.use(require('koa-logger')());
+
+// 静态资源中间件
+app.use(require('koa-static')(path.join(__dirname, './client/static'), {
+    maxage: fileCacheTime // 文件缓存时间
+}));
+
 // 请求体处理中间件
 app.use(require('koa-bodyparser')({
-    formLimit: '10mb', // 请求表单的最大体积
-    jsonLimit: '10mb', // 请求json的最大体积
-    textLimit: '10mb'  // 请求文本的最大体积
+    formLimit: queryLimit, // 请求表单的最大体积
+    jsonLimit: queryLimit, // 请求json的最大体积
+    textLimit: queryLimit  // 请求文本的最大体积
 }));
 
 
@@ -23,6 +31,6 @@ app.use(basicRouter.routes()).use(basicRouter.allowedMethods());
 
 
 // 启动服务，并输出日期
-app.listen(port, async () => {
-    console.log(`web服务启动成功！地址：127.0.0.1:${port}`);
+app.listen(port, host, async () => {
+    console.log(`web服务启动成功！地址：${host}:${port}`);
 });
