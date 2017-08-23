02fec17256c7d4c0c6585c5429b03d470f5e6e9f
yanmin.yu <yanmin.yu@mobilemd.cn>
2017-08-23 16:53:21 +0800

    '1'

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 /**
  * Created by yanmin.yu on 2017/8/5.
  */
+
+
+var darNum = (10000 * 0.086 / 365).toFixed(2);
+var benJin = 20000;
+var monthLi = 0;
+var day = 0;
+var monthSize = 0;
+
+
+while (monthLi < 2500) {
+    var dayLi = benJin * 0.086 / 365;
+
+
+    benJin += dayLi;
+    monthLi += dayLi;
+
+    if (++day === 30) {
+        monthSize++;
+        console.log('月利率-'+ monthLi)
+        console.log('本金-'+ benJin)
+        console.log('月份-'+ monthSize)
+
+
+        benJin += 5000 + monthLi;
+        monthLi = 0;
+        day = 0;
+    }
+}
+
+
+
+
+
 // const koa = require('koa');
 //
 //
@@ -13,11 +46,12 @@
 // // })
 // //
 // // app.listen(5001);
-var koa = require('koa');
-var app = new koa();
+// var koa = require('koa');
+// var app = new koa();
+//
+// app.use(function *(){
+//     this.body = 'Hello World';
+// });
+//
+// app.listen(3000);
 
-app.use(function *(){
-    this.body = 'Hello World';
-});
-
-app.listen(3000);
