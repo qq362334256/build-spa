2254c3a658864fff8f5348e4ef7675b81cdaacc0
miaoyu <362334256@qq.com>
2017-09-08 00:23:11 +0800

    '提交构建'

--- client/build/webpack.basic.js
+++ client/build/webpack.basic.js
@@ -4,13 +4,14 @@
 const path = require('path');
 
 
+// 导出webpack基本配置
 module.exports = {
     entry: path.join(__dirname, './../src', 'entry.js'), // 入口文件
 
     // 输出配置
     output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, './../src', 'entry.js'), // 输出目录
+        filename: 'bundle.js',                 // 打包的文件名
+        path: path.join(__dirname, './../static') // 输出目录
     }
 };
 
